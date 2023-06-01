af517e9a12a08d97fbf76e4854bff34fef64f27c
tongsiripath <tong.siripath@gmail.com>
2023-06-01 15:49:45 +0700

    edit server.js 4

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 app.use(cors({    
-    origin: ["https://api-goodex.onrender.com"],
+    origin: ["http://tongsiripath.mypressonline.com"],
     methods: ["POST","GET"],
     credentials: true
 }));
